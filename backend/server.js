0230246b2161853e953465dd0a1c49d25343eb9c
Desmond Kofi Adusei <desdhi24@gmail.com>
2023-06-21 21:17:51 +0000

    adding cors to api

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,20 @@
 const express = require("express");
 const bodyparser = require("body-parser");
+const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 8080;
 const appRoutes = require("./routes/index");
 app.use(bodyparser.json());
+
+app.use(cors({ origin: true, credentials: true }));
+
 app.use("/api", appRoutes);
 
 app.get("/", (req, res, next) => {
   res.status(200).json({
     message: "welcome to our  voting app",
-  }); 
+  });
 });
-app.listen(PORT, ( ) => {
+app.listen(PORT, () => {
   console.log(`server running on port${PORT}`);
 });
